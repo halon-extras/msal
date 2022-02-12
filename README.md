e74cccdc692ee5b5e2893272bcc33145305c479e
Fredrik Lengstrand <fredrik.lengstrand@halon.se>
2022-02-12 16:45:27 +0000

    Support confidential tenants

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Microsoft Authentication Library (MSAL) plugin
 
-This plugin is a wrapper for the [Microsoft Authentication Library (MSAL) for Go](https://github.com/AzureAD/microsoft-authentication-library-for-go) library. It supports fetching OAuth2 access tokens for users in Office 365.
+This plugin is a wrapper for the [Microsoft Authentication Library (MSAL) for Go](https://github.com/AzureAD/microsoft-authentication-library-for-go) library. It enables you to acquire security tokens to call protected APIs.
 
 ## Installation
 
@@ -29,11 +29,19 @@ plugins:
     config:
       tenants:
         - id: tenant1
+          type: public
           client_id: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
           authority: https://login.microsoftonline.com/organizations
           scopes:
             - https://outlook.office.com/SMTP.Send
           cache_file: /tmp/msal_tenant1_cache.json
+        - id: tenant2
+          type: confidential
+          client_id: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+          authority: https://login.microsoftonline.com/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+          scopes:
+            - https://graph.microsoft.com/.default
+          cache_file: /tmp/msal_tenant2_cache.json
 ```
 
 ## Exported functions
@@ -44,45 +52,14 @@ Fetch an OAuth2 access token using the provided credentials.
 
 **Params**
 
+- id `string` - The ID of the tenant as configured in `smtpd.yaml`
 - options `array` - Options array
 
 The following options are available in the **options** array.
 
-- id `string` - The ID of the tenant as configured in `smtpd.yaml` (See above)
-- username `string` - The username for the mailbox in Office 365
-- password `string` - The password for the mailbox in Office 365
+- username `string` - The username (Ony applicable to tenants of type `public`)
+- password `string` - The password (Ony applicable to tenants of type `public`)
 
 **Returns**
 
 A successful fetch will return an associative array with a `result` key that contains the access token. On error an associative array with a `error` key will be provided.
-
-## Example
-
-Below is an example on how to send email through Office 365 using OAuth2.
-
-### predelivery.hsl
-
-```
-$tenant = "tenant1";
-$username = "xxxx@tenant1.onmicrosoft.com";
-$password = "xxxxxxxx";
-
-$result = msal(["id" => $tenant, "username" => $username, "password" => $password]);
-
-if (!$result["result"]) {
-    Queue([
-        "reason" => "Could not fetch access token",
-        "increment_retry" => false,
-        "delay" => 60
-    ]);
-}
-
-$password = "user=".$username."\x01auth=Bearer ".$result["result"]."\x01\x01";
-
-Try([
-    "server" => "smtp.office365.com",
-    "port" => 587,
-    "saslmechanism" => "XOAUTH2",
-    "saslpassword" => $password
-]);
-```
--- msal.go
+++ msal.go
@@ -11,18 +11,27 @@ import (
 	"sync"
 	"unsafe"
 
+	"github.com/AzureAD/microsoft-authentication-library-for-go/apps/confidential"
 	"github.com/AzureAD/microsoft-authentication-library-for-go/apps/public"
 )
 
-type tenant struct {
+type public_tenant struct {
 	id     string
 	client public.Client
 	scopes []string
 }
 
+type confidential_client struct {
+	id     string
+	client confidential.Client
+	scopes []string
+	secret string
+}
+
 var (
-	tenants []tenant
-	lock    = sync.Mutex{}
+	public_tenants       []public_tenant
+	confidential_clients []confidential_client
+	lock                 = sync.Mutex{}
 )
 
 func main() {}
@@ -65,6 +74,15 @@ func Halon_init(hic *C.HalonInitContext) C.bool {
 		}
 		id := C.GoString(id_cs)
 
+		type_prop_cs := C.CString("type")
+		defer C.free(unsafe.Pointer(type_prop_cs))
+		type_cs := C.HalonMTA_config_string_get(C.HalonMTA_config_object_get(tenant_chc, type_prop_cs), nil)
+		if type_cs == nil {
+			log.Println("Missing required \"type\" setting")
+			return false
+		}
+		_type := C.GoString(type_cs)
+
 		cache_file_prop_cs := C.CString("cache_file")
 		defer C.free(unsafe.Pointer(cache_file_prop_cs))
 		cache_file_cs := C.HalonMTA_config_string_get(C.HalonMTA_config_object_get(tenant_chc, cache_file_prop_cs), nil)
@@ -82,6 +100,18 @@ func Halon_init(hic *C.HalonInitContext) C.bool {
 			return false
 		}
 		client_id := C.GoString(client_id_cs)
+
+		var client_secret string
+		if _type == "confidential" {
+			client_secret_prop_cs := C.CString("client_secret")
+			defer C.free(unsafe.Pointer(client_secret_prop_cs))
+			client_secret_cs := C.HalonMTA_config_string_get(C.HalonMTA_config_object_get(tenant_chc, client_secret_prop_cs), nil)
+			if client_secret_cs == nil {
+				log.Println("Missing required \"client_secret\" setting")
+				return false
+			}
+			client_secret = C.GoString(client_secret_cs)
+		}
 
 		authority_prop_cs := C.CString("authority")
 		defer C.free(unsafe.Pointer(authority_prop_cs))
@@ -117,13 +147,32 @@ func Halon_init(hic *C.HalonInitContext) C.bool {
 			return false
 		}
 
-		cacheAccessor := &TokenCache{file: cache_file}
-		client, err := public.New(client_id, public.WithCache(cacheAccessor), public.WithAuthority(authority))
-		if err != nil {
-			log.Println(err)
+		switch _type {
+		case "public":
+			cacheAccessor := &TokenCache{file: cache_file}
+			client, err := public.New(client_id, public.WithCache(cacheAccessor), public.WithAuthority(authority))
+			if err != nil {
+				log.Println(err)
+				return false
+			}
+			public_tenants = append(public_tenants, public_tenant{id: id, client: client, scopes: scopes})
+		case "confidential":
+			cacheAccessor := &TokenCache{file: cache_file}
+			cred, err := confidential.NewCredFromSecret(client_secret)
+			if err != nil {
+				log.Println(err)
+				return false
+			}
+			client, err := confidential.New(client_id, cred, confidential.WithAccessor(cacheAccessor), confidential.WithAuthority(authority))
+			if err != nil {
+				log.Println(err)
+				return false
+			}
+			confidential_clients = append(confidential_clients, confidential_client{id: id, client: client, scopes: scopes, secret: client_secret})
+		default:
+			log.Println("Empty or invalid \"type\" setting")
 			return false
 		}
-		tenants = append(tenants, tenant{id: id, client: client, scopes: scopes})
 
 		i++
 	}
@@ -146,7 +195,7 @@ func set_ret_value(ret *C.HalonHSLValue, key string, value string) {
 	C.HalonMTA_hsl_value_set(ret_value, C.HALONMTA_HSL_TYPE_STRING, value_cs_up, 0)
 }
 
-func get_token_by_username_and_password(tenant tenant, username string, password string) (string, error) {
+func get_token_by_username_and_password(tenant public_tenant, username string, password string) (string, error) {
 	lock.Lock()
 	defer lock.Unlock()
 
@@ -168,6 +217,20 @@ func get_token_by_username_and_password(tenant tenant, username string, password
 	return result.AccessToken, nil
 }
 
+func get_token_by_credential(tenant confidential_client) (string, error) {
+	lock.Lock()
+	defer lock.Unlock()
+
+	result, err := tenant.client.AcquireTokenSilent(context.Background(), tenant.scopes)
+	if err != nil {
+		result, err = tenant.client.AcquireTokenByCredential(context.Background(), tenant.scopes)
+		if err != nil {
+			return "", err
+		}
+	}
+	return result.AccessToken, nil
+}
+
 //export msal
 func msal(hhc *C.HalonHSLContext, args *C.HalonHSLArguments, ret *C.HalonHSLValue) {
 	var id string
@@ -179,56 +242,69 @@ func msal(hhc *C.HalonHSLContext, args *C.HalonHSLArguments, ret *C.HalonHSLValu
 
 	var args_0 = C.HalonMTA_hsl_argument_get(args, 0)
 	if args_0 != nil {
-		if C.HalonMTA_hsl_value_type(args_0) == C.HALONMTA_HSL_TYPE_ARRAY {
-			var args_0_id_cs = C.CString("id")
-			defer C.free(unsafe.Pointer(args_0_id_cs))
-			var args_0_id *C.HalonHSLValue = C.HalonMTA_hsl_value_array_find(args_0, args_0_id_cs)
-			if args_0_id == nil {
-				set_ret_value(ret, "error", "Could not find \"id\" option")
-				return
-			}
-			if !C.HalonMTA_hsl_value_get(args_0_id, C.HALONMTA_HSL_TYPE_STRING, unsafe.Pointer(&id_cs), nil) {
-				set_ret_value(ret, "error", "Invalid type of \"id\" option")
-				return
-			}
-			id = C.GoString(id_cs)
-
-			var args_0_username_cs = C.CString("username")
-			defer C.free(unsafe.Pointer(args_0_username_cs))
-			var args_0_username *C.HalonHSLValue = C.HalonMTA_hsl_value_array_find(args_0, args_0_username_cs)
-			if args_0_username == nil {
-				set_ret_value(ret, "error", "Could not find \"username\" option")
-				return
-			}
-			if !C.HalonMTA_hsl_value_get(args_0_username, C.HALONMTA_HSL_TYPE_STRING, unsafe.Pointer(&username_cs), nil) {
-				set_ret_value(ret, "error", "Invalid type of \"username\" option")
-				return
-			}
-			username = C.GoString(username_cs)
-
-			var args_0_password_cs = C.CString("password")
-			defer C.free(unsafe.Pointer(args_0_password_cs))
-			var args_0_password *C.HalonHSLValue = C.HalonMTA_hsl_value_array_find(args_0, args_0_password_cs)
-			if args_0_password == nil {
-				set_ret_value(ret, "error", "Could not find \"password\" option")
-				return
-			}
-			if !C.HalonMTA_hsl_value_get(args_0_password, C.HALONMTA_HSL_TYPE_STRING, unsafe.Pointer(&password_cs), nil) {
-				set_ret_value(ret, "error", "Invalid type of \"password\" option")
-				return
-			}
-			password = C.GoString(password_cs)
-		} else {
-			set_ret_value(ret, "error", "Invalid type of \"options\" argument")
+		if !C.HalonMTA_hsl_value_get(args_0, C.HALONMTA_HSL_TYPE_STRING, unsafe.Pointer(&id_cs), nil) {
+			set_ret_value(ret, "error", "Invalid type of \"id\" option")
 			return
 		}
+		id = C.GoString(id_cs)
 	} else {
-		set_ret_value(ret, "error", "Missing required \"options\" argument")
+		set_ret_value(ret, "error", "Missing required \"id\" argument")
 		return
 	}
-	for _, tenant := range tenants {
+
+	for _, tenant := range public_tenants {
 		if tenant.id == id {
+			var args_1 = C.HalonMTA_hsl_argument_get(args, 1)
+			if args_1 != nil {
+				if C.HalonMTA_hsl_value_type(args_1) == C.HALONMTA_HSL_TYPE_ARRAY {
+					var args_1_username_cs = C.CString("username")
+					defer C.free(unsafe.Pointer(args_1_username_cs))
+					var args_1_username *C.HalonHSLValue = C.HalonMTA_hsl_value_array_find(args_1, args_1_username_cs)
+					if args_1_username == nil {
+						set_ret_value(ret, "error", "Could not find \"username\" option")
+						return
+					}
+					if !C.HalonMTA_hsl_value_get(args_1_username, C.HALONMTA_HSL_TYPE_STRING, unsafe.Pointer(&username_cs), nil) {
+						set_ret_value(ret, "error", "Invalid type of \"username\" option")
+						return
+					}
+					username = C.GoString(username_cs)
+
+					var args_1_password_cs = C.CString("password")
+					defer C.free(unsafe.Pointer(args_1_password_cs))
+					var args_1_password *C.HalonHSLValue = C.HalonMTA_hsl_value_array_find(args_1, args_1_password_cs)
+					if args_1_password == nil {
+						set_ret_value(ret, "error", "Could not find \"password\" option")
+						return
+					}
+					if !C.HalonMTA_hsl_value_get(args_1_password, C.HALONMTA_HSL_TYPE_STRING, unsafe.Pointer(&password_cs), nil) {
+						set_ret_value(ret, "error", "Invalid type of \"password\" option")
+						return
+					}
+					password = C.GoString(password_cs)
+				} else {
+					set_ret_value(ret, "error", "Invalid type of \"options\" argument")
+					return
+				}
+			} else {
+				set_ret_value(ret, "error", "Missing required \"options\" argument")
+				return
+			}
+
 			token, err := get_token_by_username_and_password(tenant, username, password)
+			if err != nil {
+				set_ret_value(ret, "error", err.Error())
+				return
+			}
+
+			set_ret_value(ret, "result", token)
+			return
+		}
+	}
+
+	for _, tenant := range confidential_clients {
+		if tenant.id == id {
+			token, err := get_token_by_credential(tenant)
 			if err != nil {
 				set_ret_value(ret, "error", err.Error())
 				return
