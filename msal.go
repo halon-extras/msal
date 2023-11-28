4f5178295273a2456338d1aeccfe6f0f0f8b2bfa
Fredrik Lengstrand <fredrik.lengstrand@halon.se>
2023-11-28 22:13:08 +0000

    Change order of functions

--- msal.go
+++ msal.go
@@ -53,77 +53,6 @@ var (
 
 func main() {}
 
-func GetConfigAsJSON(cfg *C.HalonConfig) (string, error) {
-	var x *C.char
-	y := C.HalonMTA_config_to_json(cfg, &x, nil)
-	defer C.free(unsafe.Pointer(x))
-	if y {
-		return C.GoString(x), nil
-	} else {
-		if x != nil {
-			return "", errors.New(C.GoString(x))
-		} else {
-			return "", errors.New("failed to get config")
-		}
-	}
-}
-
-func GetArgumentAsString(args *C.HalonHSLArguments, pos uint64, required bool) (string, error) {
-	var x = C.HalonMTA_hsl_argument_get(args, C.ulong(pos))
-	if x == nil {
-		if required {
-			return "", fmt.Errorf("missing argument at position %d", pos)
-		} else {
-			return "", nil
-		}
-	}
-	var y *C.char
-	if C.HalonMTA_hsl_value_get(x, C.HALONMTA_HSL_TYPE_STRING, unsafe.Pointer(&y), nil) {
-		return C.GoString(y), nil
-	} else {
-		return "", fmt.Errorf("invalid argument at position %d", pos)
-	}
-}
-
-func GetArgumentAsJSON(args *C.HalonHSLArguments, pos uint64, required bool) (string, error) {
-	var x = C.HalonMTA_hsl_argument_get(args, C.ulong(pos))
-	if x == nil {
-		if required {
-			return "", fmt.Errorf("missing argument at position %d", pos)
-		} else {
-			return "", nil
-		}
-	}
-	var y *C.char
-	z := C.HalonMTA_hsl_value_to_json(x, &y, nil)
-	defer C.free(unsafe.Pointer(y))
-	if z {
-		return C.GoString(y), nil
-	} else {
-		return "", fmt.Errorf("invalid argument at position %d", pos)
-	}
-}
-
-func SetReturnValueToAny(ret *C.HalonHSLValue, val interface{}) error {
-	x, err := json.Marshal(val)
-	if err != nil {
-		return err
-	}
-	y := C.CString(string(x))
-	defer C.free(unsafe.Pointer(y))
-	var z *C.char
-	if !(C.HalonMTA_hsl_value_from_json(ret, y, &z, nil)) {
-		if z != nil {
-			err = errors.New(C.GoString(z))
-			C.free(unsafe.Pointer(z))
-		} else {
-			err = errors.New("failed to parse return value")
-		}
-		return err
-	}
-	return nil
-}
-
 //export Halon_version
 func Halon_version() C.int {
 	return C.HALONMTA_PLUGIN_VERSION
@@ -305,4 +234,75 @@ func Halon_hsl_register(hhrc *C.HalonHSLRegisterContext) C.bool {
 	C.HalonMTA_hsl_register_function(hhrc, msal_cs, nil)
 	C.HalonMTA_hsl_module_register_function(hhrc, msal_cs, nil)
 	return true
+}
+
+func GetConfigAsJSON(cfg *C.HalonConfig) (string, error) {
+	var x *C.char
+	y := C.HalonMTA_config_to_json(cfg, &x, nil)
+	defer C.free(unsafe.Pointer(x))
+	if y {
+		return C.GoString(x), nil
+	} else {
+		if x != nil {
+			return "", errors.New(C.GoString(x))
+		} else {
+			return "", errors.New("failed to get config")
+		}
+	}
+}
+
+func GetArgumentAsString(args *C.HalonHSLArguments, pos uint64, required bool) (string, error) {
+	var x = C.HalonMTA_hsl_argument_get(args, C.ulong(pos))
+	if x == nil {
+		if required {
+			return "", fmt.Errorf("missing argument at position %d", pos)
+		} else {
+			return "", nil
+		}
+	}
+	var y *C.char
+	if C.HalonMTA_hsl_value_get(x, C.HALONMTA_HSL_TYPE_STRING, unsafe.Pointer(&y), nil) {
+		return C.GoString(y), nil
+	} else {
+		return "", fmt.Errorf("invalid argument at position %d", pos)
+	}
+}
+
+func GetArgumentAsJSON(args *C.HalonHSLArguments, pos uint64, required bool) (string, error) {
+	var x = C.HalonMTA_hsl_argument_get(args, C.ulong(pos))
+	if x == nil {
+		if required {
+			return "", fmt.Errorf("missing argument at position %d", pos)
+		} else {
+			return "", nil
+		}
+	}
+	var y *C.char
+	z := C.HalonMTA_hsl_value_to_json(x, &y, nil)
+	defer C.free(unsafe.Pointer(y))
+	if z {
+		return C.GoString(y), nil
+	} else {
+		return "", fmt.Errorf("invalid argument at position %d", pos)
+	}
+}
+
+func SetReturnValueToAny(ret *C.HalonHSLValue, val interface{}) error {
+	x, err := json.Marshal(val)
+	if err != nil {
+		return err
+	}
+	y := C.CString(string(x))
+	defer C.free(unsafe.Pointer(y))
+	var z *C.char
+	if !(C.HalonMTA_hsl_value_from_json(ret, y, &z, nil)) {
+		if z != nil {
+			err = errors.New(C.GoString(z))
+			C.free(unsafe.Pointer(z))
+		} else {
+			err = errors.New("failed to parse return value")
+		}
+		return err
+	}
+	return nil
 }
